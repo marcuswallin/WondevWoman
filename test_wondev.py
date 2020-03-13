48e9edbcd110ac4e63d34097f26c9f5a3e673a7a
StyleBoss <marcus.wallin93@outlook.com>
2020-03-13 14:27:34 +0100

    improved the mapreader function, but still far from done

--- test_wondev.py
+++ test_wondev.py
@@ -1,6 +1,6 @@
 import pytest
 import sys
-from wondevwoman import Cell, Game
+from wondevwoman import Cell, Game, Map
 
 def test_simple_point_init():
     cell = Cell(1,2,0)
@@ -13,32 +13,62 @@ class GameTest:
     def __init__(self, start_input_file, loop_input_file):
         self.start_input_file = start_input_file 
         self.loop_input_file = loop_input_file
+        self.init_game_from_file()
 
     def init_game_from_file(self):
         self.game = Game()
-        self.insert_init_data_from_file_to_instream()
+        self.insert_data_from_file_to_instream(self.start_input_file)
         self.read_game_data()
         
     def read_game_data(self):
         stdin_ = sys.stdin
         self.game.read_start_input()
         sys.stdin = stdin_ 
+    
+    def update_loop_data_from_file(self):
+        self.insert_data_from_file_to_instream(self.loop_input_file)
+        self.read_loop_data_from_stream()
 
-    def insert_init_data_from_file_to_instream(self):
-        input_file = open(self.start_input_file, "r", encoding="utf-8")
+    def insert_data_from_file_to_instream(self, file_name):
+        input_file = open(file_name, "r", encoding="utf-8")
         sys.stdin = input_file
+
+    def read_loop_data_from_stream(self):
+        stdin_ = sys.stdin
+        self.game.read_game_data()
+        sys.stdin = stdin_ 
 
 
 def test_game_init():
     game = Game()
-    assert game.size == 0
+    assert game.map.size == 0
     assert game.units_per_player == 0
 
-def test_file_init_game():
-    game_test = GameTest("wondev_test_files/size6unit1.csv", "wondev_test_files/loop_data.csv")
-    game_test.init_game_from_file()
-    assert game_test.game.size == 6
+def test_file_init_game_simple():
+    game_test = GameTest("wondev_test_files/size6unit1.csv", "wondev_test_files/loop_size6unit1.csv")
+    assert game_test.game.map.size == 6
     assert game_test.game.units_per_player == 1
+
+    game_test.update_loop_data_from_file()
+    curr_map = game_test.game.map
+    assert curr_map.get_height(0,0) == 0
+    assert curr_map.get_height(1,0) == 0
+    assert curr_map.get_height(2,0) == 1
+    assert curr_map.get_height(3,0) == 0
+    assert curr_map.get_height(4,0) == 0
+    assert curr_map.get_height(5,0) == 0
+    assert curr_map.get_height(0,4) == 0
+    assert curr_map.get_height(1,4) == 0
+    assert curr_map.get_height(2,4) == 1
+    assert curr_map.get_height(3,4) == 0
+    assert curr_map.get_height(4,4) == 2
+    assert curr_map.get_height(5,4) == 0
+    assert curr_map.get_height(5,5) == 0
+
+
+
+
+
 
 
 
--- wondevwoman.py
+++ wondevwoman.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from array import array
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -24,31 +25,59 @@ class Unit:
         self.x = x
         self.y = y
 
+class Map:
+    def __init__(self, size):
+        self.size = size
+        self.cells = []
+
+    def get_height(self, x, y):
+        return self.cells[y][x]
+    
+    def print(self):
+        for row in self.cells:
+            for cell in row:
+                print(cell, end = ' ', file=sys.stderr)
+            print("", file=sys.stderr)
+
 class Game:
     def __init__(self):
-        self.size = 0
+        self.map = Map(0)
         self.units_per_player = 0
     
     def read_start_input(self):
-        self.size = int(input())
+        self.map.size = int(input())
         self.units_per_player = int(input())
+
 
 #TODO split function into smaller ones function
     def read_game_data(self):
-        for i in range(self.size):
-            row = input()
-            print(row, file=sys.stderr)
+        self.read_map_data()
+
+        #TODO currently dummy input, should be Player objects
+        self.read_player_data(1)
+        self.read_player_data(2)
+
+        self.read_legal_actions()
+
+    def read_player_data(self, player):
         for i in range(self.units_per_player):
             unit_x, unit_y = [int(j) for j in input().split()]
-            print(str(unit_x) +" "+str(unit_y), file=sys.stderr)
-        for i in range(self.units_per_player):
-            other_x, other_y = [int(j) for j in input().split()]
-            print(str(other_x)+" "+str(other_y), file=sys.stderr)
+
+    def read_legal_actions(self):
         legal_actions = int(input())
         for i in range(legal_actions):
             atype, index, dir_1, dir_2 = input().split()
             index = int(index)
-            print(f'{atype} {index} {dir_1} {dir_2}', file=sys.stderr)
+
+    #TODO should let map object to most work
+    #TODO Cells should really be cells, not strings as they are now
+    def read_map_data(self):
+        for i in range(self.map.size):
+            row = input().split()
+            self.map.cells.append([])
+            for cell in row:
+                self.map.cells[i].append(cell)
+
 
 def main():
     game = Game()
